49bb78a79d0dcd3d522f403ac73fc5f102b972a3
Yona Appletree <hypher@gmail.com>
2025-12-18 17:20:37 -0800

    chore: clean up Terraform provider block formatting in versions.tf

--- terraform/aws/versions.tf
+++ terraform/aws/versions.tf
@@ -2,9 +2,8 @@ terraform {
   required_version = ">= 1.11"
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "> 6"
-      configuration_aliases = [aws.us-east-1]
+      source  = "hashicorp/aws"
+      version = "> 6"
     }
     random = {
       source  = "hashicorp/random"
